7275a8a72c4c643d3fd593f6a7883c5f1ab1c19e
krikchaip <krikchai.p@gmail.com>
2019-04-22 19:06:32 +0700

    main content container

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import Grid from '@material-ui/core/Grid'
+import Paper from '@material-ui/core/Paper'
+import withStyles from '@material-ui/core/styles/withStyles'
 
-function App() {
+/** @type {import('@material-ui/core/styles').StyleRules} */
+const styles = {
+  root: {
+    padding: '3rem'
+  },
+  container: {
+    height: '25rem'
+  }
+}
+
+/**
+ * @param {object} props
+ * @param {Object<string, string>} props.classes
+ */
+function App({ classes }) {
   return (
-    <div>
-      Hello world!
-    </div>
+    <React.Fragment>
+      <CssBaseline />
+      <Grid container justify="center" className={classes.root}>
+        <Grid item xs={8}>
+          <Paper className={classes.container}>
+          </Paper>
+        </Grid>
+      </Grid>
+    </React.Fragment>
   )
 }
 
-export default App
+export default withStyles(styles)(App)
